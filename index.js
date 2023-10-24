d4df24522e7d33cd686c4c97ef764c48695a8b46
Christina Kozanian <christinakozanian@gmail.com>
2023-10-23 17:29:36 -0700

    adds json file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-document.addEventListener('DOMContentLoaded', ()=>console.log('conected'))
+document.addEventListener('DOMContentLoaded', ()=>{
+    getJoke()
+})
+
+function getJoke() {
+    fetch('https://official-joke-api.appspot.com/random_ten')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+}
