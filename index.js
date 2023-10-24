5905bcb7308af6043b8c878d79defa5af9320038
Christina Kozanian <christinakozanian@gmail.com>
2023-10-24 14:02:58 -0700

    prints error message if punchline does not match

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let jokesData
     getJokes().then(data => {
         jokesData = data
-        //logFirstJoke(jokesData)
         keyPressed(jokesData)
     })
 })
@@ -12,16 +11,6 @@ function getJokes() {
     .then(res => res.json())
     .then(data => data)
 }
-/*
-function logFirstJoke(jokesData){
-    let h2 = document.createElement('h2')
-    let label = document.querySelector('#joke-header')
-    h2.textContent = jokesData[0].setup
-    label.appendChild(h2)
-    const punchline = jokesData[0].punchline
-    console.log(punchline)
-    checkInput(punchline)
-}*/
 
 function keyPressed(jokesData){
     let currentIndex = 0
@@ -56,13 +45,17 @@ function checkInput(punchline){
     let userPunchline = document.querySelector('input#punchline').value
     let alteredPunchline = punchline.toLowerCase().slice(0,-1)
     const form = document.querySelector('#form')
-    const message = document.createElement('p')
-    form.appendChild(message)
+    let message = form.querySelector('p')
+    if (!message){
+        message = document.createElement('p')
+        form.appendChild(message)
+    }
     if (userPunchline.toLowerCase() === alteredPunchline){
-        console.log('yes')
         message.textContent = 'You got it!'
     }
     else if (userPunchline.toLowerCase() !== alteredPunchline){
-        message.textContent = 'Do you give up?'
+        message.textContent = 'Not quite! Do you give up?'
     }
 }
+
+//add something that will loop over the input multiple times
