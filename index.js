bb7c5f1d278454e4f613dd0f720dacd31cada192
Christina Kozanian <christinakozanian@gmail.com>
2023-10-23 19:56:51 -0700

    update keyPressed function to log joke setups

--- index.js
+++ index.js
@@ -5,30 +5,32 @@ document.addEventListener('DOMContentLoaded', ()=>{
 })
 
 function getJokes() {
-    fetch('http://localhost:3000/jokes')
+    return fetch('http://localhost:3000/jokes')
     .then(res => res.json())
-    .then(data => {console.log(data)})
+    .then(data => data)
 }
 
 function keyPressed(){
     document.addEventListener('keydown', (event)=>{
-        if (event.key === 'ArrowRight') {
-            console.log('right')
-        }
+        getJokes().then(data => {
+            console.log(data)
+            data.forEach(joke => {
+                if (event.key === 'ArrowRight') {
+                    console.log('right')
+                    console.log(joke.setup)
+                    }
+            })
+        })
     })
 }
 
 function iterateJokeSetup() {
     fetch('http://localhost:3000/jokes')
     .then(res => res.json())
-    .then(data => {
-        data.map(joke => {
-            const setup = joke.setup
-            let p = document.createElement('p')
-            p.textContent = setup
-            let label = document.querySelector('#joke-header')
-            label.appendChild(p)
-        })
-    })
+    .then(data => data)
 }
 
+//let p = document.createElement('p')
+            //p.textContent = setup
+            //let label = document.querySelector('#joke-header')
+            //label.appendChild(p)
