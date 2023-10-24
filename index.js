4e9da1c313aefe36d0ac456cda68dca61c20a7d1
Christina Kozanian <christinakozanian@gmail.com>
2023-10-23 20:26:01 -0700

    update keyPressed function to change the joke setup instead of creating a new tag for each one

--- index.js
+++ index.js
@@ -17,16 +17,17 @@ function keyPressed(){
         jokesData = data
         console.log(jokesData)
         })
+    let p = document.createElement('p')
+    let label = document.querySelector('#joke-header')
+    label.appendChild(p)
+
     document.addEventListener('keydown', (event)=>{
         if (event.key === 'ArrowRight' && jokesData) {
             if (currentIndex < jokesData.length) {
                 jokesData.forEach((joke, index) => {
                     if (index === currentIndex){
                         console.log(joke.setup)
-                        let p = document.createElement('p')
                         p.textContent = joke.setup
-                        let label = document.querySelector('#joke-header')
-                        label.appendChild(p)
                         }
                 });
                 currentIndex++
